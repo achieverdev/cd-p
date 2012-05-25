47a5ac0b54dfb404787668d5ba3052dfeb342bfc
achiever <achiever.dev@gmail.com>
2012-05-25 16:24:05 +0530

    product categories

--- backup/TcustomDesign/CustomDesignService.php
+++ backup/TcustomDesign/CustomDesignService.php
@@ -50,30 +50,7 @@ class CustomDesignService
 	}
 	
 	public function getProdcutColorOptions($param)
-	{
-		/*
-		$this->connect();		
-		$query = "SELECT * FROM tbl_product_variant WHERE product_id=$param";
-		$result = (mysql_query($query)) or die ('getProdcutColorOptions->query problem'.mysql_error()) ;	
-		
-		$recordObjArr = array();
-	        while ($row = mysql_fetch_object($result)) 
-			{	
-				$fetchObj['id']						 	 = $row->id;
-				$fetchObj['product_id'] 				 = $row->product_id;
-				$fetchObj['front_image'] 				 = $row->front_image;
-				$fetchObj['back_image'] 				 = $row->back_image;
-				$fetchObj['subcategories_id']			 = $row->subcategories_id;
-				$fetchObj['size_id']					 = $row->size_id;
-				$fetchObj['color_id']					 = $row->color_id;
-							
-				$recordObjArr [] 				 	 	 = $fetchObj;				
-       		}
-	        
-			 mysql_free_result($result);
-				 
-	    return $recordObjArr;		
-		*/		
+	{		
 		$this->connect();		
 		$query = "SELECT * FROM tbl_product_variant WHERE product_id=$param";
 		$result = (mysql_query($query)) or die ('getProdcutColorOptions->query problem'.mysql_error()) ;	
@@ -104,21 +81,23 @@ class CustomDesignService
 	public function getProductCategrories()
 	{
 		$this->connect();		
-		$query 			= "SELECT * FROM tbl_product";
-		$result 		= (mysql_query($query)) or die ('getProdcutColorOptions->query problem'.mysql_error()) ;	
+		$query 			= "SELECT * FROM tbl_subcategories";
+		$result 		= (mysql_query($query)) or die ('getProductCategrories->query problem'.mysql_error()) ;	
 		
 		$recordObjArr   = array();
 		while ($row 	= mysql_fetch_object($result)) 
 		{	
-			$fetchObj['id']						 = $row->id;
-			$fetchObj['product_id'] 			 = $row->product_id;
-			$fetchObj['front_image'] 			 = $row->front_image;
-			$fetchObj['back_image'] 			 = $row->back_image;
-			$fetchObj['subcategories_id']		 = $row->subcategories_id;
-			$fetchObj['size_id']				 = $row->size_id;
-			$fetchObj['color_id']				 = $row->color_id;
-							
-			$recordObjArr [] 				 	 = $fetchObj;				
+			$fetchObj['id']						 	= $row->id;
+			$fetchObj['name'] 			 			= $row->name;
+			$fetchObj['category_id'] 			 	= $row->category_id;
+			$fetchObj['description'] 			 	= $row->description;							
+			// categories details
+			$categories_query  	= "SELECT * FROM tbl_categories WHERE id=$row->category_id";		
+			$categories_result 	= mysql_query($categories_query) or die ('getProductCategrories->categories_query'.mysql_error()) ;				
+			$categories_row 	= mysql_fetch_object($categories_result);
+			$fetchObj['category_name']				= $categories_row->name;			
+						
+			$recordObjArr [] 				 	 	= $fetchObj;				
 		}	
 		mysql_free_result($result);
 				 
@@ -146,7 +125,7 @@ class CustomDesignService
 			$colorcode_row 	  = mysql_fetch_object($colorcode_result);
 			$fetchObj['color_code']					 = $colorcode_row->code;	
 							
-			$recordObjArr [] 				 	 = $fetchObj;				
+			$recordObjArr [] 				 	 	 = $fetchObj;				
 		}	
 		mysql_free_result($result);
 				 
